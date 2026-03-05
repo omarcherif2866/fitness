99cf006109a8b0853f568962a6ea9f4a44e9f61c
Omar Cherif <omarcherif2866@gmail.com>
2026-03-05 14:21:59 +0100

    Add logout

--- angular/src/app/navbar/navbar.component.ts
+++ angular/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,27 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   userId!: any;
+    isLoggedIn: boolean = false;
+
+  constructor(private router: Router) {}
+
   ngOnInit() {
     this.userId = localStorage.getItem('userId'); // Replace with the actual method to get the user ID
+    this.checkAuth();
+  }
+
+  checkAuth(): void {
+    const token = localStorage.getItem('accessToken');
+    this.isLoggedIn = !!token;
+    this.userId = localStorage.getItem('userId');
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('userAuth');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userRole');
+    this.isLoggedIn = false;
+    this.router.navigate(['/home']);
   }
 }
